3da00ae1a82bb90f793909f20ff639f8a07d2aea
Justin Kunimune <justinkunimune@gmail.com>
2019-07-30 16:42:19 -0400

    I broke it.
    I added the location finding function. It doesn't work yet. Ha.

--- Programming/Bill.c
+++ Programming/Bill.c
@@ -7,11 +7,32 @@
 #include <Wire.h>
 #include <Main.h>
 
+#define SUN_MEMORY_SIZE 9
+#define NUM_INTERP_POINTS 9
+
 DS3231 Clock;
+
+long sunrises[SUN_MEMORY_SIZE]; // the last few sunrises
+long sunsets[SUN_MEMORY_SIZE]; // the last few sunsets
+int orbit; // the index of the current rise-to-rise orbit (the zeroth is the incomplete one that includes launch)
+
+
+struct Location {
+  float latitude;
+  float longitude;
+  float altitude;
+}
+
 
 void setup(){
   begin();
+
+  for (int i = 0; i < SUN_MEMORY_SIZE; i ++) {
+    sunrises[i] = -1;
+    sunsets[i] = -1;
+  }
 }
+
 
 void loop(){
   pos = sync();
@@ -21,6 +42,7 @@ void loop(){
     deorbit();
   }
 }
+
 
 /*_________FUNCTIONS USED IN BOTH BILL AND BURT__________*/
 
@@ -82,4 +104,62 @@ void deorbit(){
     PARAMETERS: None
     RETURN: None
   */
+}
+
+void getCoordinates(long t){
+  /*
+    FUNCTION: Estimates the location at the given time
+    PARAMETERS: t, the time of interest
+    RETURN: {latitude, longitude, altitude}
+    PRECONDITION: t should be in the current orbit for a semblance of accuracy
+  */
+  float time_0[]; // load expected coordinates from SD card
+  float latitude_0[];
+  float longitude_0[];
+  float altitude_0[];
+
+  long sunsetsExp[NUM_ORBITS]; // load expected observations from SD card
+  long sunrisesExp[NUM_ORBITS];
+
+  float latCoefs[NUM_ORBITS][NUM_INTERP_POINTS][2*SUN_MEMORY_SIZE]; // load parameter coefficients
+  float lonCoefs[NUM_ORBITS][NUM_INTERP_POINTS][2*SUN_MEMORY_SIZE];
+  float altCoefs[NUM_ORBITS][NUM_INTERP_POINTS][2*SUN_MEMORY_SIZE];
+  float rtcCoefs[NUM_ORBITS][NUM_INTERP_POINTS][2*SUN_MEMORY_SIZE];
+
+  float latInterp[NUM_INTERP_POINTS];
+  for (int k = 0; k < NUM_INTERP_POINTS; k ++)
+    latInterp[k] = 0;
+  float lonInterp[NUM_INTERP_POINTS];
+  for (int k = 0; k < NUM_INTERP_POINTS; k ++)
+    lonInterp[k] = 0;
+  float altInterp[NUM_INTERP_POINTS];
+  for (int k = 0; k < NUM_INTERP_POINTS; k ++)
+    altInterp[k] = 0;
+  float driftGuess = 0;
+
+  for (int l = 0; l < SUN_MEMORY_SIZE; l ++) {
+    int j = orbit - SUN_MEMORY_SIZE + l;
+    if (orbit - SUN_MEMORY_SIZE + l >= 0) {
+      for (int k = 0; k < NUM_INTERP_POINTS; j ++) {
+        latInterp[k] += latCoefs[orbit][k][2*l]   * (sunsets[l] - sunsetsExp[j]);
+        latInterp[k] += latCoefs[orbit][k][2*l+1] * (sunrises[l] - sunrisesExp[j]);
+        lonInterp[k] += lonCoefs[orbit][k][2*l]   * (sunsets[l] - sunsetsExp[j]);
+        lonInterp[k] += lonCoefs[orbit][k][2*l+1] * (sunrises[l] - sunrisesExp[j]);
+        altInterp[k] += altCoefs[orbit][k][2*l]   * (sunsets[l] - sunsetsExp[j]);
+        altInterp[k] += altCoefs[orbit][k][2*l+1] * (sunrises[l] - sunrisesExp[j]);
+        driftGuess += rtcCoefs[orbit][2*l]   * (sunsets[l] - sunsetsExp[j]);
+        driftGuess += rtcCoefs[orbit][2*l+1] * (sunrises[l] - sunrisesExp[j]);
+      }
+    }
+  }
+
+  float tInterp[NUM_INTERP_POINTS];
+  for (int k = 0; k < NUM_INTERP_POINTS; k ++)
+    tInterp[k] = (sunrisesExp[orbit-1]-sunrisesExp[orbit])/(NUM_INTERP_POINTS-1)*k + sunrisesExp[orbit-1];
+
+  Location loc;
+  loc.latitude  = linterp(time_0, latitude_0,  t - driftGuess) + splinterp(tInterp, latInterp, t - driftGuess);
+  loc.longitude = linterp(time_0, longitude_0, t - driftGuess) + splinterp(tInterp, lonInterp, t - driftGuess);
+  loc.altitude  = linterp(time_0, altitude_0,  t - driftGuess) + splinterp(tInterp, altInterp, t - driftGuess);
+  return loc;
 }
