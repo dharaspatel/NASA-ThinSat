d8fa765116d6a3e0242fdab229a53d5e28dc43c1
Max Schommer <maxschommer@gmail.com>
2019-08-01 16:00:16 -0400

    Did a review on Joe.c

--- Programming/Joe.c
+++ Programming/Joe.c
@@ -1,3 +1,16 @@
+
+// Overall Comments 
+// Make a CMakeLists.txt file and use cmake, or make a makefile (no extension, just create a new file called makefile or Makefile) in order to compile your code. The makefile describes how your files are related to eachother
+//    https://www.cs.swarthmore.edu/~newhall/unixhelp/howto_makefiles.html#creating
+// All of your .c files should have a .h file → For example, Joe.c should have a Joe.h file. These header files should contain prototypes for all of the functions/classes in the .c file. 
+//    https://www.tutorialspoint.com/cprogramming/c_header_files
+// Your code for different satellites should probably be in different folders each with their own makefile so it’s easy to load onto their respective boards. 
+// Don’t ever include .c files → They should each have a .h file that you include
+//    See lines "#include "ReadPhotocells.c""
+// You need to be very careful about blocking operations. You have a lot of them in this file. The main loop needs to run reliably and not be blocked for long.
+
+
+
 /*  ________________________________________________
     Description:  file that controls all electronics on joe
     Author: Dhara Patel
@@ -31,12 +44,12 @@
 #include "TSLPB.h" //NSL BUS
 #include "myDataPacketStructure.h" //datapacket stuff
 #include "Arduino.h" //Arduino library
-#include "ReadPhotocells.c" //reading photocells
-#include "ImgProcessing.c"
+#include "ReadPhotocells.c" //reading photocells                              // Only include .h files, not .c
+#include "ImgProcessing.c"                                                    // Same here
 
 DS3231 Clock;
-EEPROM eeprom;
-DateTime rtcTime; //the current time output by the rtc
+EEPROM eeprom;                                                                // Not sure where the eeprom type comes from
+DateTime rtcTime; //the current time output by the rtc                        // I think this comes from RTClib, which isn't included
 TSLPB tslpb; //bus
 UserDataStruct_t missionData; //packet of mission data
 float secLaunch; //seconds since launch
@@ -44,32 +57,32 @@ struct pho_data{
   long sunrises[];
   long sunsets[];
 }; //sunset sunrise data
-bool firstRun = true; //first photocell run
-int state; //nothing = 0, launch = 1, pyrolysis = 2
+bool firstRun = true; //first photocell run                                   // Consider renaming to isFirstRun or something that indicates it's a state
+int state; //nothing = 0, launch = 1, pyrolysis = 2                           // state is a very generic name, make it more specific. Long variable names aren't bad if it's more clear
 float threshold[]; //the threshold for the sunset/sunrise calculations
-int launchCount = 0; //the number of launchers released
-bool motor = true; //using motor design?
+int launchCount = 0; //the number of launchers released                       // All launchers are released at once, so this doesn't do what it's supposed to
+bool motor = true; //using motor design?                                      // Again, not clear that this is a state. You should do a #define MOTOR, and then use ifdef statements to see if that's the current implementation
 size_t mag_data[]; //an array of x, y, z magnetometer data
 struct position{
   float seconds;
   float latitude;
   float longitude;
   float altitude;
-  bool moonless;
-  float  inHalfOrbit[3]; //lat, long, alt in half orbit
+  bool moonless;                                                              // <---  Why is this part of the postion struct? It seems to be derived...
+  float  inHalfOrbit[3]; //lat, long, alt in half orbit                       // <-╵
 }; //sec, lat, long, alt
 struct py_data{
-  size_t temperature;
+  size_t temperature;                                                          // Why is a size_t used here instead of a float or int? It makes the code less clear
   int histogram[10];
 } //sensor data from pyrolysis experiment
 
-void setup(){
+void setup(){                                                                  // Begin shouldn't exist. Just put everything in begin in setup. 
   begin();
 }
 
 void loop(){
   rtcTime = Clock.now();
-  calibrateTime(rtcTime);
+  calibrateTime(rtcTime);    
   readPhotocells(firstRun);
   position = getPosition(rtcTime, pho_data);
   state = calc_state(position);
@@ -79,9 +92,12 @@ void loop(){
       //j.chilling
       delay(1);
       break;
-    case 1:
+    case 1:                                                                     // They should all have break statements after stuff is done
       //begin launch process
-      launch(motor);
+      launch(motor);                                                            // Also here the motor should be an ifdef rather than a parameter
+                                                                                // You launch all of the targets at once. We need an additional sub state
+                                                                                // to specify which one to launch. We're launching in 4 consecutive orbits 
+                                                                                // and need launch to be a non-blocking operation so that postiiton can update. 
     case 2:
       //begin pyrolysis process
       pyrolysis();
@@ -93,7 +109,8 @@ void loop(){
 
 /*___FUNCTIONS___*/
 
-void begin(){
+void begin(){                                                                   // The clock needs to have it's true time set at some point with a separate script, which never happens. 
+                                                                                // Every other operation (when the satellite is actually running) should just read the clock
   /*
     FUNCTION: Begins communication with I2C interface, starts the DS3231 Clock, writes table to SD
     PARAMETERS: None
@@ -101,22 +118,23 @@ void begin(){
   */
   tslpb.begin();
   Wire.begin();
-  pinMode(ClockPowerPin, OUTPUT);
+  pinMode(ClockPowerPin, OUTPUT);                                               // ClockPowerPin doesn't exist... and even if it did you should never need to power the
+                                                                                // clock on. It should always be on...
   digitalWrite(ClockPowerPin, HIGH);
   tslpb.InitTSLDigitalSensors();
   rtcTime = Clock.now();
 }
 
-float calibrateTime(DateTime rtcTime){
+float calibrateTime(DateTime rtcTime){                                          // You don't return anything in this function, so it should be void
   /*
     FUNCTION: Uses forcast.csv to calculate seconds from launch and sync a more accurate DateTime
-    PARAMETERS: None
+    PARAMETERS: None                                                           // This isn't true. Your doc strings are all out of date it seems
     RETURN: Seconds since launch
   */
 
-  Clock.setSecond(sec_calc);
-  Clock.setMinute(min_calc);
-  Clock.setHour(hour_calc);
+  Clock.setSecond(sec_calc);                                                   // I don't know what these are. sec_calc etc. don't seem to exist
+  Clock.setMinute(min_calc);                                                   // The functions setSecond, setMinute etc. also don't seem to exist (from 
+  Clock.setHour(hour_calc);                                                    // what I've seen of the DS3231 library, although I could be missing something)
   Clock.setDate(date_calc);
   Clock.setMonth(mon_calc);
   Clock.setYear(yr_calc);
@@ -130,11 +148,11 @@ struct readPhotocells(bool firstRun){
     RETURN: a struct with sunset/sunrise data
   */
   return ReadPhotocells.main(firstRun)
-  firstRun = false;
+  firstRun = false;                                                            // This will never get called since it's after the return statement.
 }
 
 
-float getPosition(DateTime rtcTime, struct pho_data){
+float getPosition(DateTime rtcTime, struct pho_data){                          // Again, see below ↴
   /*
     FUNCTION: Uses forcast.csv to
     PARAMETERS: None
@@ -142,7 +160,8 @@ float getPosition(DateTime rtcTime, struct pho_data){
   */
 }
 
-int calc_state(struct position){
+int calc_state(struct position){                                               // You can't not have no argument name. "postition" is the type, not the name of the argument.
+                                                                               // Try something like "struct position pos" where pos is the argument name
   /*
     FUNCTION: Calculates the state of the satellite depending on the position
     PARAMETERS: the pos
@@ -152,7 +171,7 @@ int calc_state(struct position){
   float satAngle = getTheta();
   if(satAngle <= sightAngle){
       return 1;
-      launchCount ++;
+      launchCount ++;                                                           // Again, this never gets incremented.
   }else if(launchCount == 4 && position.moonless){
     //insert pyrolysis code
   }else{
@@ -161,10 +180,12 @@ int calc_state(struct position){
 
 }
 
-void sendData(){
+void sendData(){                                                                // This should maybe return a success/failure status, and have a timeout default argument
   /*
     FUNCTION: sends data to the bus
-    PARAMETERS: the number corresponding to the struct variable you are sending
+    PARAMETERS: the number corresponding to the struct variable you are sending // This parameter doesn't exist. 
+                                                                                // There should also be a parameter for the data. Pulling "missionData" from globals is
+                                                                                // bad practice
     RETURN: None
   */
   while (!tslpb.isClearToSend()){
@@ -173,17 +194,19 @@ void sendData(){
   tslpb.pushDataToNSL(missionData);
 }
 
-void readSD(char file_name[]){
+void readSD(char file_name[]){                                                
   /*
     FUNCTION: Returns all of the contents of a file
     PARAMETERS: the file name of the SD card you want to read
     RETURN: None
   */
-  File data_file = SD.open(file_name);
-  while(data_file.avaliable()){
-    return data_file.read();
+  File data_file = SD.open(file_name);                                         // You need to do SD.begin(CS_PIN_NUM) before reading the SD card
+  while(data_file.avaliable()){                                                // This loop runs once
+    return data_file.read();                                                   // This will return one line. Also, returning an entire file
+                                                                               // is not possible with the amount of ram we have. You should have a way to look
+                                                                               // up a specific time/pose in a file to see it's value
   }
-  data_file.close();
+  data_file.close();                                                           // This never gets called
 }
 
 void writeEEPROM(size_t data, int address){
@@ -194,7 +217,9 @@ void writeEEPROM(size_t data, int address){
   */
 }
 
-size_t readEEPROM(int address){
+size_t readEEPROM(int address){                                                 // This function doesn't seem to work. First, why a size_t? Second the amount of bytes to 
+                                                                                // read isn't specified (unless you just want to read one byte. Third, a EEPROM.read seems like
+                                                                                // the exact same function, so why not just use that? 
   /*
     FUNCTION: read an address on the eeprom
     PARAMETERS: the address you want to read from
@@ -209,11 +234,11 @@ size_t readMag(){
     PARAMETERS: None
     RETURN: data one after the other
   */
-  magx = tslpb.readTSLDigitalSensorRaw(Magnetometer_x);
+  magx = tslpb.readTSLDigitalSensorRaw(Magnetometer_x);                         // magx was never declared/has no type. Also, why does this function return a size_t and not an array?
   magy = tslpb.readTSLDigitalSensorRaw(Magnetometer_y);
   magz = tslpb.readTSLDigitalSensorRaw(Magnetometer_z);
 
-  return magx, magy, magz;
+  return magx, magy, magz;                                                      // A function can only return one value, so this won't work.
 }
 
 void launch(bool motor){
@@ -222,12 +247,12 @@ void launch(bool motor){
     PARAMETERS: boolean for which launcher design is being used (motor or rifled design)
     RETURN: None
   */
-  if(motor){
+  if(motor){                                                                     // Replace with ifdef here
     digitalWrite(IN1_ADDR, HIGH); //motor driver is going forward
     digitalWrite(IN2_ADDR, LOW);
-  }
-
-  melt([HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW, LOW, LOW, LOW]);
+  }                    
+                                                                                 // There needs to be a delay to wait for the motor to spin up
+  melt([HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW, LOW, LOW, LOW]);                // We're not  going melt more than one wire at a time
 }
 
 void pyrolysis(){
@@ -236,10 +261,11 @@ void pyrolysis(){
     PARAMETERS: None
     RETURN: None
   */
-
+                                                                                 // Again, one melt at a time, and in consecutive orbits.
   melt([LOW, LOW, LOW, LOW, LOW, LOW, HIGH, HIGH, HIGH, HIGH]);
-  readTemp();
-  ImgProcessing.main(readImg());
+  readTemp();                                                                    // This doesn't do anything (the temperature returned isn't used)
+  ImgProcessing.main(readImg());                                                 // This also doens't do anything... the information needs to be sent
+                                                                                 // somewhere. 
 }
 
 void melt(int wires[]){
@@ -249,7 +275,11 @@ void melt(int wires[]){
     RETURN: None
   */
 
-  for(int i = 0, i < 10, i++){
+  for(int i = 0, i < 10, i++){                                                    // The magic number 10 should be passed into the function (we're not going to
+                                                                                  // be melting more than one wire at a time anyways)
+                                                                                  // In all events, you should probably use this library or something like it
+                                                                                  // anyways
+                                                                                  // https://github.com/Simsso/ShiftRegister74HC595
     digitalWrite(SER_ADDR, wires[i])
     digitalWrite(SRCLK_ADDR, HIGH);
     digitalWrite(SRCLK_ADDR, LOW);
@@ -257,7 +287,8 @@ void melt(int wires[]){
   digitalWrite(RCLK_ADDR, HIGH);
   digitalWrite(RCLK_ADDR, LOW);
 
-  delay(10000);
+  delay(10000);                                                                    // What does this do? You should clarify that this is the melt time and make it an 
+                                                                                   // argument for the function
   for(int i = 0, i < 10, i++){
     digitalWrite(SER_ADDR, LOW);
     digitalWrite(SRCLK_ADDR, HIGH);
@@ -282,12 +313,14 @@ size_t readTemp(){
     PARAMETERS: None
     RETURN: data (not processed)
   */
-  size_t v_out;
+  size_t v_out;                                                                 // This shouldn't be a size_t, but a uint or an int 
+                                                                                // (since it's read from the Analog to Digital Converter which is 10 bit)
+                                                                                // https://www.tutorialspoint.com/cprogramming/c_data_types
 
-  Wire.requestFrom(TEMP_ADDR,1); //is 1 the correct number of bits?
+  Wire.requestFrom(TEMP_ADDR,1); //is 1 the correct number of bits?             // Why are you using wire? It's not an I2C device... just read the voltage
   while(Wire.avaliable()){
     v_out = Wire.read();
   }
 
-  return (v_out - 400)/19.5; //see pg. 2 of datasheet --> http://ww1.microchip.com/downloads/en/DeviceDoc/20001942G.pdf
+  return (v_out - 400)/19.5; //see pg. 2 of datasheet --> http://www.microchip.com/downloads/en/DeviceDoc/20001942G.pdf
 }
