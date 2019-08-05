c683c8766df4c06c949e44c7104f3669b7b38972
Justin Kunimune <justinkunimune@gmail.com>
2019-08-05 16:39:17 -0400

    It's a drag race
    I told it to vary its drag area as well as initial conditions. I also made my syntax better and added print statements.

--- Programming/location_timing/change_initcond.py
+++ Programming/location_timing/change_initcond.py
@@ -19,7 +19,6 @@ initial_epoch = '01 Nov 2020 13:00:00.000'
 uncertainty   = 60 # days
 output_dir = '../../simulations/' # >> where txt files will be saved
 script_dir = './' # >> where to find original scripts
-noon, midnight,  sunrise = False, False, True # >> events to find location
 
 import pdb
 import numpy as np
@@ -33,6 +32,8 @@ assert shutil.which("GMAT") is not None, "You need GMAT on your path, friend."
 
 # ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
+np.random.seed(0)
+
 # >> initial conditions bsed on Jack Bacon's ThinSat_PlanningPropuulsive.script
 init_v = np.array([4.504094411355064, 3.530755928387845, -5.28883890677512])
 init_pos = np.array([5313.19898722968, -2912.368416712065, 2580.583006475884])
@@ -40,14 +41,16 @@ init_pos = np.array([5313.19898722968, -2912.368416712065, 2580.583006475884])
 # >> generate new velocity with small random offset
 velocity = init_v + np.random.normal(0, v_scale, (samples, 3))
 position = init_pos + np.random.normal(0, r_scale, (samples, 3))
+area = np.exp(np.random.uniform(np.log(.001), np.log(.01), samples))
 line_num_epoch = 11
+line_num_drag = 33
 line_nums_v = [19, 20, 21]
 line_nums_pos = [16, 17, 18]
 line_nums_out = [160, 243, 259]
 
 # >> run simulations for each date
 for d in range(uncertainty):
-    
+    print("Day {}:".format(d))
     day = '{:02d}'.format(int(initial_epoch.split()[0]) + d)
     epoch = initial_epoch.split()
     epoch[0] = day
@@ -57,32 +60,27 @@ for d in range(uncertainty):
     for i in range(samples):
         with open(script_dir + 'ThinSat_simulation.script', 'r') as f:
             lines = f.readlines()
-            # >> change initial velocity vector
-            for j in range(3):
-                if i != 0: # >> 0th simultation has init conds
-                    new = lines[line_nums_v[j]].split()
-                    new[2] = str(velocity[i,j]) + '\n'
-                    lines[line_nums_v[j]] = ' '.join(new)
-                    new = lines[line_nums_pos[j]].split()
-                    new[2] = str(position[i,j]) + '\n'
-                    lines[line_nums_pos[j]] = ' '.join(new)
+            if i != 0: # >> 0th simultation has init conds
+                # >> change initial velocity and position vector
+                for j in range(3):
+                    lines[line_nums_v[j]] = "DefaultSC.V{} = {:f};\n".format("XYZ"[j], velocity[i,j])
+                    lines[line_nums_pos[j]] = "DefaultSC.{} = {:f};\n".format("XYZ"[j], position[i,j])
+                # >> change drag area
+                lines[line_num_drag] = "GMAT DefaultSC.DragArea = {:.3f};\n".format(drag[i])
 
             # >> change start time
             lines[line_num_epoch] = "GMAT DefaultSC.Epoch = '{}';\n".format(epoch)
 
             # >> change report file location
-            new = lines[line_nums_out[0]].split()
-            new[3] = "'{}SunriseSunset_d{:02d}_{:03d}.txt';\n".format(output_dir, d, i)
-            lines[line_nums_out[0]] = ' '.join(new)
-            new = lines[line_nums_out[1]].split()
-            new[3] = "'{}ReportFile1_d{:02d}_{:03d}.txt';\n".format(output_dir, d, i)
-            lines[line_nums_out[1]] = ' '.join(new)
-            new = lines[line_nums_out[2]].split()
-            new[3] = "'{}ReportFile2.txt';\n".format(output_dir, d, i)
-            lines[line_nums_out[2]] = ' '.join(new)
+            lines[line_nums_out[0]] = "GMAT SunriseSunset.Filename = '{}SunriseSunset_d{:02d}_{:03d}.txt';\n".format(output_dir, d, i)
+            lines[line_nums_out[1]] = "GMAT ReportFile1.Filename = '{}ReportFile1_d{:02d}_{:03d}.txt';\n".format(output_dir, d, i)
+            lines[line_nums_out[2]] = "GMAT ReportFile2.Filename = '{}ReportFile2_d{:02d}_{:03d}.txt';\n".format(output_dir, d, i)
 
         # >> run gmat simulation
         script = output_dir + 'temp.script'
         with open(script, 'w') as f:
             f.write(''.join(lines))
+
+        print("Running simulation {}".format(i))
         subprocess.run([shutil.which('GMAT'), script, '--minimize'])
+        print("Done!")
