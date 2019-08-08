ac7ebf28a5e0d69d076ef35298d30439676d3547
Justin Kunimune <justinkunimune@gmail.com>
2019-08-08 15:51:05 -0400

    PDDCPINDDDDCDBPRC
    It didn't have a pin layout for this one. I wonder what I'm doing.

--- Programming/SDTest/SDTest/sd_raw_config.h
+++ Programming/SDTest/SDTest/sd_raw_config.h
@@ -106,12 +106,20 @@ extern "C"
 
     #define select_card() PORTB &= ~(1 << PORTB0)
     #define unselect_card() PORTB |= (1 << PORTB0)
+#elif defined(__AVR_ATmega32U4__)
+	#define configure_pin_mosi() DDRB |= (1 << DDB2)
+    #define configure_pin_sck() DDRB |= (1 << DDB1)
+    #define configure_pin_ss() DDRB |= (1 << DDB0)
+    #define configure_pin_miso() DDRB &= ~(1 << DDB3)
+
+    #define select_card() PORTB &= ~(1 << PORTB0)
+    #define unselect_card() PORTB |= (1 << PORTB0)
 #else
     #error "no sd/mmc pin mapping available!"
 #endif
 
-#define configure_pin_available() DDRC &= ~(1 << DDC4)
-#define configure_pin_locked() DDRC &= ~(1 << DDC5)
+#define configure_pin_available() DDRC &= ~(1 << DDC6)
+#define configure_pin_locked() DDRC &= ~(1 << DDC7)
 
 #define get_pin_available() (PINC & (1 << PINC4))
 #define get_pin_locked() (PINC & (1 << PINC5))
